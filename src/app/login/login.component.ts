d6df8d0d6b206217fc3e514035e46f2d6166ebe8
dinamagdyrashed <dinarashid78@gmail.com>
2024-09-17 16:55:00 +0300

    res

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { AuthService } from '../services/auth.service';
 import { Router, RouterLink } from '@angular/router';
 
@@ -8,30 +13,39 @@ import { Router, RouterLink } from '@angular/router';
   standalone: true,
   imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent implements OnInit {
-  constructor(private _AuthService: AuthService, private _Router: Router) { }
+  constructor(private _AuthService: AuthService, private _Router: Router) {}
   loginForm = new FormGroup({
     email: new FormControl(null, [Validators.required, Validators.email]),
-    password: new FormControl(null, [Validators.required, Validators.minLength(6), Validators.maxLength(20)])
-  })
+    password: new FormControl(null, [
+      Validators.required,
+      Validators.minLength(6),
+      Validators.maxLength(20),
+    ]),
+  });
   phoneImage: string = '';
   invalidLogin: string = '';
-
+  loading: boolean = false;
   login(formData: FormGroup) {
+    this.loading = true;
     this._AuthService.login(formData.value).subscribe({
       next: (res) => {
         if (res.token) {
-          localStorage.setItem('user', res.token)
-          this._AuthService.saveCurrentUser()
+          localStorage.setItem('user', res.token);
+          this._AuthService.saveCurrentUser();
+          this.loading = false;
         }
-        this._Router.navigate(['/home'])
-      }, error: (err) => {
-        this.invalidLogin = err.error.message
-      }
-    })
+        this._Router.navigate(['/home']);
+      },
+      error: (err) => {
+        this.invalidLogin = err.error.message;
+      },
+    });
   }
 
-  ngOnInit(): void { this.phoneImage = this._AuthService.loginPhoto }
+  ngOnInit(): void {
+    this.phoneImage = this._AuthService.loginPhoto;
+  }
 }
